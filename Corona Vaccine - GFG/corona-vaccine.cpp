c5b03f844b5f3ef8247bb390830b05c1c7382394
Nitin _Tayal <67607648+nitintayal008@users.noreply.github.com>
2022-02-17 11:38:46 +0530

    Added solution - LeetHub

---
+++ Corona Vaccine - GFG/corona-vaccine.cpp
@@ -0,0 +1,152 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node 
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) 
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+class Solution{
+public:
+        //we will simplly iterate over postorder i.e. from bottom and we will take care of 
+        //three major points:
+        //we need camera when we have leaf node
+        //if u need  camera ---->-1
+        //if u have a camera---->0
+        //if u are covered by camera---->1
+        //and we will count if we need camera
+     int vacine=0;
+    int helper(Node* root){
+       
+        if(root==NULL)
+        return 1;
+        
+        int lc=helper(root->left);
+        int rc=helper(root->right);
+        
+        if(lc==-1||rc==-1){
+            vacine++;
+            return 0;
+        }
+        if(lc==0||rc==0)
+        return 1;
+        
+        return -1;
+    }
+    int supplyVaccine(Node* root){
+        // Your code goes here
+        if(helper(root)==-1)
+        vacine++;
+        
+        return vacine;
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string treeString;
+        getline(cin, treeString);
+        Node *root = buildTree(treeString);
+        Solution ob;
+        cout<<ob.supplyVaccine(root)<<"\n";
+    }
+    return 0;
+}
+
+
+
+  // } Driver Code Ends
