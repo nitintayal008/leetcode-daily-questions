15d056f5226274c64482156c70d991bb323c2b63
Nitin _Tayal <67607648+nitintayal008@users.noreply.github.com>
2022-02-22 18:44:06 +0530

    Time: 5 ms (22.87%), Space: 5.9 MB (67.10%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+    
+    //example BCD
+    //26*26*2 + 26*3 + 4
+    //26(26*2+3)+4
+    //result*26+d
+    
+    
     int titleToNumber(string columnTitle) {
           int result = 0;
         for (char c: columnTitle){
