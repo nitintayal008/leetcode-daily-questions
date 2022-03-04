1ee4631c1e4ed98a03d024ada19fd9ec201a2745
Nitin _Tayal <67607648+nitintayal008@users.noreply.github.com>
2022-03-04 22:56:19 +0530

    Time: 16 ms (51.90%), Space: 6.1 MB (75.19%) - LeetHub

---
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        double glass[101][101] = {0};
+        glass[0][0] = poured;
+        for(int i = 0; i<100; i++){
+            for(int j = 0; j<=i; j++){
+                if(glass[i][j] >= 1){
+                    glass[i + 1][j] += (glass[i][j] - 1) / 2;
+                    glass[i + 1][j + 1] += (glass[i][j] - 1) / 2; 
+                    glass[i][j] = 1;
+                }
+            }
+        }
+        return glass[query_row][query_glass];
+    }
+};
