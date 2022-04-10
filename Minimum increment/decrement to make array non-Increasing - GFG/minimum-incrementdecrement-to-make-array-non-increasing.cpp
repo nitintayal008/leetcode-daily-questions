96bacc6658ac78baa4ec73be060ecfe07ffb36e7
Nitin _Tayal <67607648+nitintayal008@users.noreply.github.com>
2022-04-10 18:16:38 +0530

    Added solution - LeetHub

---
+++ Minimum increment/decrement to make array non-Increasing - GFG/minimum-incrementdecrement-to-make-array-non-increasing.cpp
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+ // } Driver Code Ends
+
+
+class Solution{
+public:
+
+//we will use reverse priory queue in order to miantain the decreasing order 
+//everytime while iterting we compare array element with priooty quque top 
+//element and if it is greater then top element then we decrement the value 
+//upto top element and increment count by that nummber;
+    int minOperations(int *a,int n)
+    {
+        int count =0;
+        priority_queue<int ,vector<int>,greater<int>>pq;
+        for(int i=0; i<n; i++){
+            if(!pq.empty() && pq.top()<a[i]){
+                count+=a[i]-pq.top();
+                pq.push(a[i]);
+                pq.pop();
+                
+            }
+            pq.push(a[i]);
+            //3
+            //1 3
+            //1 3 5
+            //3 5
+            
+        }
+        return count;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int  a[n+1];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+            
+        Solution ob;
+        cout<<ob.minOperations(a,n)<<endl;
+    }
+    
+	return 0;
+}  // } Driver Code Ends
