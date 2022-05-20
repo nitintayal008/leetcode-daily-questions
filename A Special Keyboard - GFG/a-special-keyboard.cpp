2c70064a7b37b0f088e5c9a6dbda2c103b99a0ec
Nitin _Tayal <67607648+nitintayal008@users.noreply.github.com>
2022-05-20 18:09:03 +0530

    Added solution - LeetHub

---
+++ A Special Keyboard - GFG/a-special-keyboard.cpp
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int findTime(string S1, string S2) {
+        // code here
+        int curr=0;
+       
+        int temp=0,sum=0;
+        for(int j=0; j<S2.size(); j++){
+            curr=S1.find(S2[j]);
+            sum+=abs(curr-temp);
+            temp=curr;
+        }
+        return sum;
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S1,S2;
+        cin>>S1;
+        cin>>S2;
+
+        Solution ob;
+        cout<<ob.findTime(S1,S2)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
