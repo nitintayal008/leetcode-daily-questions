02fcce38fa77a377e7ac785a32085aedd1168681
Nitin _Tayal <67607648+nitintayal008@users.noreply.github.com>
2022-06-28 00:09:43 +0530

    Added solution - LeetHub

--- Change Bits - GFG/change-bits.cpp
+++ Change Bits - GFG/change-bits.cpp
@@ -9,23 +9,27 @@ using namespace std;
 
 class Solution {
   public:
-    vector<int> changeBits(int N) {
-        int store = N;
-        int len = 0;
-        int B;
-        vector<int> ans;
-        while(N) {
-            N /= 2;
-            len++;
+    vector<int> changeBits(int n) {
+        // code here
+        int i=0;
+        int sum=0;
+        int k=n;
+        vector<int>ans;
+        while(n){
+            int temp=n%2;
+            n=n/2;
+            if(temp==0){
+                sum+=1*pow(2,i++);
+            }else{
+                sum+=1*pow(2,i++);
+            }
         }
-        int g = pow(2, len) - 1;
-        int dif = abs(g - store);
-        ans.push_back(dif);
-        ans.push_back(g);
+        ans.push_back(sum-k);
+        ans.push_back(sum);
+        
         return ans;
     }
 };
-
 
 // { Driver Code Starts.
 int main() {
