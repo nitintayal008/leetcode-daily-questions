6338125c4f1bb08bb9ddfba88db28b8c63ef045c
Nitin _Tayal <67607648+nitintayal008@users.noreply.github.com>
2022-08-12 19:49:33 +0530

    Time: 24 ms (54.44%), Space: 14.3 MB (57.51%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -27,3 +27,26 @@ public:
         }
     }
 };
+//this below solution is more intresting solution in o(H) time.
+//
+// Let large = max(p.val, q.val), small = min(p.val, q.val).
+// We keep iterate root in our BST:
+// If root.val > large then both node p and q belong to the left subtree, go to left by root = root.left.
+// If root.val < small then both node p and q belong to the right subtree, go to right by root = root.right.
+// Now, small <= root.val <= large the current root is the LCA between q and p.
+// class Solution {
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         int small = min(p->val, q->val);
+//         int large = max(p->val, q->val);
+//         while (root != nullptr) {
+//             if (root->val > large) // p, q belong to the left subtree
+//                 root = root->left;
+//             else if (root->val < small) // p, q belong to the right subtree
+//                 root = root->right;
+//             else // Now, small <= root.val <= large -> This root is the LCA between p and q
+//                 return root;
+//         }
+//         return nullptr;
+//     }
+//};
