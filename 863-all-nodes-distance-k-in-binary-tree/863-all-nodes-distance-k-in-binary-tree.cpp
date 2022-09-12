9e723cce57244cade77995a39f761d7c1300753c
Nitin _Tayal <67607648+nitintayal008@users.noreply.github.com>
2022-09-12 16:52:42 +0530

    Time: 16 ms (23.12%), Space: 12.9 MB (62.48%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -8,66 +8,60 @@
  * };
  */
 class Solution {
-    void markBothside(TreeNode* root,unordered_map<TreeNode*,TreeNode*> &parent,TreeNode* target){
+public:
+    void dfs(TreeNode* root,unordered_map<TreeNode*,TreeNode*>&parent){
         queue<TreeNode*>q;
         q.push(root);
+        //vector<parent>pt;
         while(!q.empty()){
-            TreeNode* temp=q.front();
-            q.pop();
-            if(temp->left) {
-                parent[temp->left]=temp;
-                q.push(temp->left);
-                }
-            if(temp->right){
-                parent[temp->right]=temp;
-                q.push(temp->right);
-            }
+            //int size=q.size();
             
+                TreeNode* temp=q.front();
+                q.pop();
+                if(temp->left){
+                    parent[temp->left]=temp;
+                    q.push(temp->left);
+                }
+                if(temp->right){
+                    parent[temp->right]=temp;
+                    q.push(temp->right);
+                }
         }
     }
-
-public:
+    
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        //first we will make tree such that it travere in both the direction
-        //so for that we will use unorderd_map to store double sided link 
         unordered_map<TreeNode*,TreeNode*>parent;
-        markBothside(root,parent,target);
-        unordered_map<TreeNode*, bool>vis;
-        //after we get link to both side we will start from the target and go in all the three direction and count the distance ...for that we will maintain visited array so we can go in respected dirction
+        dfs(root,parent);
+        unordered_map<TreeNode*,bool>visited;
         queue<TreeNode*>q;
         q.push(target);
-        vis[target]=true;
-        int count=0;
+        visited[target]=true;
+        int level=0;
         while(!q.empty()){
             int size=q.size();
-            if(count++ ==k)break;
-            // count++;
-            
+            if(level++ ==k)break;
             for(int i=0; i<size; i++){
                 TreeNode* curr=q.front();
                 q.pop();
-                if(curr->left && !vis[curr->left]){
+                if(curr->left && !visited[curr->left]){
                     q.push(curr->left);
-                    vis[curr->left]=true;
+                    visited[curr->left]=true;
                 }
-                if(curr->right && !vis[curr->right]){
+                if(curr->right && !visited[curr->right]){
                     q.push(curr->right);
-                    vis[curr->right]=true;
+                    visited[curr->right]=true;
                 }
-                if(parent[curr] && !vis[parent[curr]]){
+                if(parent[curr] && !visited[parent[curr]]){
                     q.push(parent[curr]);
-                    vis[parent[curr]]=true;
+                    visited[parent[curr]]=true;;
                 }
             }
-           
         }
-        vector<int>ans;
+        vector<int>result;
         while(!q.empty()){
-            TreeNode* curr = q.front();
-            q.pop();
-            ans.push_back(curr->val);
+            TreeNode* curr=q.front();q.pop();
+            result.push_back(curr->val);
         }
-        return ans;
+        return result;
     }
-        
 };
