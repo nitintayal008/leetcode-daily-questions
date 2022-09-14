6bbf724818152e70712dd73bc3e4efb288554f7e
Nitin _Tayal <67607648+nitintayal008@users.noreply.github.com>
2022-09-14 19:17:39 +0530

    Time: 2 ms (76.84%), Space: 7 MB (96.10%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,21 +1,45 @@
 class Solution {
 public:
-    //this is the simple recurssion code we are doing
-    //taking ths nums set and new vector by taking one element of nums and not taking recursively
-    void recurFunc(int index,vector<int>&nums,vector<vector<int>>&vec,vector<int>&v){
-        if(index==nums.size()){
-            vec.push_back(v);
+   void solve(vector<int>& nums, int index,vector<vector<int>>&ans,vector<int>&temp){
+        if(nums.size()==index){
+            ans.push_back(temp);
             return;
         }
-        v.push_back(nums[index]);
-        recurFunc(index+1,nums,vec,v);
-         v.pop_back();
-        recurFunc(index+1,nums,vec,v);
+       temp.push_back(nums[index]);
+       solve(nums,index+1,ans,temp);
+       temp.pop_back();
+      // temp.push_back(nums[index]);
+       solve(nums,index+1,ans,temp);
+       
     }
+    
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>vec;
-        vector<int>v;
-        recurFunc(0,nums,vec,v);
-        return vec;
+        vector<vector<int>>ans;
+        vector<int>temp;
+        solve(nums,0,ans,temp);
+        return ans;
     }
 };
+// class Solution {
+// public:
+//     vector<vector<int>> ans;
+    
+//     void sub(vector<int> &nums, int i, vector<int> temp)
+//     {
+//         if(i==nums.size())
+//         {
+//             ans.push_back(temp);
+//             return;
+//         }
+        
+//         sub(nums, i+1, temp);
+//         temp.push_back(nums[i]);
+//         sub(nums, i+1, temp);
+//     }
+    
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<int> temp;       
+//         sub(nums, 0, temp); // or sub(nums, 0, vector<int> {});
+//         return ans;
+//     }
+// };
