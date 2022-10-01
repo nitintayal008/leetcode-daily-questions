30efd3e4a02bd8fae7367c679cc435ce7e84ad49
Nitin _Tayal <67607648+nitintayal008@users.noreply.github.com>
2022-10-01 17:25:03 +0530

    Time: 7 ms (42.42%), Space: 12.2 MB (42.58%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,49 +12,36 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        if(!root)
-        {
-            return { };
-        }
-        vector<vector<int>> result; //To store the final traversals
-        queue<TreeNode*> q; //To perform level order traversal and store the nodes level wise
+        vector<vector<int>>ans;
+        
+        if(root==NULL)return ans;
+        bool flag=true;
+        queue<TreeNode*>q;
         q.push(root);
-        vector<int> l = {root->val};
-        result.push_back(l); 
-        int flag = 0; //Variable to keep a check on levels so as to perfom reversal
-        while(!q.empty())
-        {
-            int s = q.size();
-            vector<int> level;
-            for(int i=0; i<s; i++)
-            {
-                TreeNode* node = q.front();
-                q.pop();
-                if(node->left)
-    	        {
-    	            level.push_back(node->left->val);
-    	            q.push(node->left);
-    	        }
-    	        if(node->right)
-    	        {
-    	            level.push_back(node->right->val);
-    	            q.push(node->right);
-    	        }
-            }
-            if(flag==0) //If it's an odd row number(1, 3...) (assuming row numbering is from 0)
-            {
-                reverse(level.begin(), level.end()); //We reverse that row
-                flag = 1; //We change the flag parity
-            }
-            else //We do nothing
-            {
-                flag = 0; //We change the flag parity
-            }
-            if(!level.empty()) //We check whether the level is having certain values
-            {
-                result.push_back(level); //We push the level into the result matrix
-            }
-        }
-        return result;
+       while(!q.empty()){
+           vector<int>ans1;
+           int n=q.size();
+           for(int i=0;i<n; i++){
+               TreeNode* temp=q.front();
+               q.pop();
+               if(temp->left){
+                   q.push(temp->left);
+               }
+               if(temp->right){
+                   q.push(temp->right);
+               }
+               ans1.push_back(temp->val);
+               
+           }
+           if(flag==true){
+               ans.push_back(ans1);
+               flag=false;
+           }else{
+               reverse(ans1.begin(),ans1.end());
+               ans.push_back(ans1);
+               flag=true;
+           }
+       }
+        return ans;
     }
 };
