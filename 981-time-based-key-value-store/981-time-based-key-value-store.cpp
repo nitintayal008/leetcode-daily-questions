9610e405682b19c0ec87dff71eb0e3389fe74bb0
Nitin _Tayal <67607648+nitintayal008@users.noreply.github.com>
2022-10-06 17:16:24 +0530

    Time: 694 ms (33.85%), Space: 130.7 MB (29.45%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -1,61 +1,61 @@
-class TimeMap {
-public:
-    unordered_map<string,vector<pair<int,string>>>mp;
-    TimeMap() {
-        
-    }
-    
-    void set(string key, string value, int timestamp) {
-        mp[key].push_back({timestamp,value});
-    }
-    
-    string get(string key, int timestamp) {
-       
-        
-        if(mp.find(key)==mp.end()){
-            return "";
-        }
-        
-        int l=0;
-        int r=mp[key].size()-1;
-        
-        if(mp[key][0].first>timestamp){
-            return "";
-        }
-        string ans;
-        while(l<=r){
-            
-            int mid=l +(r-l)/2;
-            if(mp[key][mid].first==timestamp){
-                return mp[key][mid].second;
-            }else if(mp[key][mid].first<timestamp){
-                ans=mp[key][mid].second;
-                l=mid+1;
-            }else{
-                r=mid-1;
-            }
-            
-        }
-        return ans;
-    }
-};
-
-//solution with unordered_map inside map
 // class TimeMap {
 // public:
-//     unordered_map<string,map<int,string>>m;
+//     unordered_map<string,vector<pair<int,string>>>mp;
 //     TimeMap() {
         
 //     }
     
 //     void set(string key, string value, int timestamp) {
-//         m[key][timestamp]=value;
+//         mp[key].push_back({timestamp,value});
 //     }
     
 //     string get(string key, int timestamp) {
-//         auto it=m[key].upper_bound(timestamp);
-//         if(it==m[key].begin())return "";
-//         it--;
-//         return it->second;
+       
+        
+//         if(mp.find(key)==mp.end()){
+//             return "";
+//         }
+        
+//         int l=0;
+//         int r=mp[key].size()-1;
+        
+//         if(mp[key][0].first>timestamp){
+//             return "";
+//         }
+//         string ans;
+//         while(l<=r){
+            
+//             int mid=l +(r-l)/2;
+//             if(mp[key][mid].first==timestamp){
+//                 return mp[key][mid].second;
+//             }else if(mp[key][mid].first<timestamp){
+//                 ans=mp[key][mid].second;
+//                 l=mid+1;
+//             }else{
+//                 r=mid-1;
+//             }
+            
+//         }
+//         return ans;
 //     }
 // };
+
+//solution with unordered_map inside map
+class TimeMap {
+public:
+    unordered_map<string,map<int,string>>m;
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        m[key][timestamp]=value;
+    }
+    
+    string get(string key, int timestamp) {
+        auto it=m[key].upper_bound(timestamp);
+        if(it==m[key].begin())return "";
+        it--;
+        return it->second;
+    }
+};
