fd6bd4eee404622c7fa003ee47263b4d0fffae11
Nitin _Tayal <67607648+nitintayal008@users.noreply.github.com>
2022-10-20 15:39:48 +0530

    Time: 19 ms (41.37%), Space: 6 MB (77.21%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string thousand[]={"","M","MM","MMM"};
+        string hundred[]={"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string tens[]={"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string unit[]={"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        
+        return thousand [num / 1000] + hundred [(num%1000)/100]+ tens[(num%100)/10] + unit[num%10];
+    }
+};  
