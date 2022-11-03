e5154ba77b8d78e687e10cba3ec1ba3fa6843a19
Nitin _Tayal <67607648+nitintayal008@users.noreply.github.com>
2022-11-03 15:45:58 +0530

    Time: 992 ms (30.39%), Space: 167.9 MB (60.00%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -7,24 +7,24 @@ public:
     //     return str;
     // }
     int longestPalindrome(vector<string>& words) {
-        int count = 0;
-       unordered_map<string,int> hm; 
-       
-       for(string s:words){ // match to make pairs that can be taken in palindrome
-           string p = s;
-           reverse(p.begin(),p.end());
-           if(hm[p]>0){ // reverse of current string found, these 2 strings can be added to palindrome 
-               count+= 4;
-               hm[p]--;  // remove occurence of string as we used it for palindrome
-           }
-           else{
-               hm[s]++;   // reverse not fount , put it in hashmap
-           }
-       }
+        unordered_map<string,int>mp;
+        int count=0;
+        int n=words.size();
+        // int first=0;
+        for(int i=0;i<n; i++){
+            string p=words[i];
+            reverse(p.begin(),p.end());
+            if(mp[p]>0){
+                count+=4;
+                mp[p]--;
+            }else{
+                 mp[words[i]]++;
+            }
+        }
         
-       for(auto it:hm){
+       for(auto it:mp){
            if(it.first[0]==it.first[1] && it.second>0){
-             return count+=2;
+               return count+2;
            }
        }
         return count;
