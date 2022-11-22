bb262a890f4a21282df106e0b62e5f0880299677
Nitin _Tayal <67607648+nitintayal008@users.noreply.github.com>
2022-11-22 22:16:18 +0530

    Time: 385 ms (54.08%), Space: 9 MB (69.87%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int>dp(n+1,INT_MAX);
+        dp[0]=0;
+        for(int nums=1;nums<=n;++nums){
+            int minCnt = INT_MAX;
+            for (int num = 1; num <= sqrt(nums); num++)
+            {
+                int curCnt = 1 + dp[nums - (num*num)];
+                minCnt = min(minCnt, curCnt);
+            }
+            dp[nums] = minCnt;
+        }
+        return dp[n];
+    }
+};
